be814f6a703441dc182c4a22b7a3d9e312b21c46
EC2 Default User <ec2-user@ip-172-31-87-3.ec2.internal>
2022-07-23 04:19:39 +0000

    curls: post api_auth/user_jwt

---
+++ src/curls/business-api/post+api_auth+user_jwt.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+. ./.env_local
+
+curl $URL/api_auth/user_jwt?prueba=1 \
+	-X POST \
+	-H "Accept: application/json" \
+	-H "Authorization: Bearer $TOKEN" \
+	-H "x-platform-name: spring" \
+	-d '{
+		"user_id":4305,
+		"authSecret":"sarlanga"
+	}'
+
