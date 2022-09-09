37265e9b2261eecc6bbdf7595356775ae58aec17
Hector Hernandez <hectorfh@gmail.com>
2022-09-09 02:10:53 +0000

    curls added

--- src/curls/business-api/post+api_auth+user_jwt.sh
+++ src/curls/business-api/post+api_auth+user_jwt.sh
@@ -6,7 +6,7 @@ curl $URL/api_auth/user_jwt?prueba=1 \
 	-X POST \
 	-H "Accept: application/json" \
 	-H "Authorization: Bearer $TOKEN" \
-	-H "x-platform-name: spring" \
+	-H "x-platform-name: ipartners" \
 	-d '{
 		"user_id":4305,
 		"authSecret":"sarlanga"
