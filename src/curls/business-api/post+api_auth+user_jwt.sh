65e17753e7c92f6dad16be3df68b02862529b8cb
Hector Hernandez <hectorfh@gmail.com>
2023-09-20 18:15:26 +0000

    fix

--- src/curls/business-api/post+api_auth+user_jwt.sh
+++ src/curls/business-api/post+api_auth+user_jwt.sh
@@ -5,7 +5,7 @@
 curl $URL/api_auth/user_jwt?prueba=1 \
 	-X POST \
 	-H "Accept: application/json" \
-	-H "Authorization: Bearer $TOKEN" \
+	-H "Proxy-Authorization: Bearer $TOKEN" \
 	-H "x-platform-name: ipartners" \
 	-d '{
 		"user_id":4305,
